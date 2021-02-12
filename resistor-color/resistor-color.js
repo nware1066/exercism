61b5f55525f41e8b324788c053abdd74a9f2f109
Naomi Ware <nware1066@yahoo.com>
2021-02-12 15:03:44 -0700

    Add resistor-color file

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,19 @@
+
+
+export const colorCode = (color) => {
+  let value = COLORS.indexOf(color)
+  return value;
+};
+
+export const COLORS = [
+     'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white'
+];
